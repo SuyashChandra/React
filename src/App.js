ffc05259b4a4abf21c5f3afae90cba14c5c718a8
Suaysh Chandra <suyash.chandra24@gmail.com>
2020-10-27 23:04:32 +0530

    The Krappa Machine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Number from "./Number";
+import Machine from "./Machine";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Standard React Layout. POG!</h1>
-        <Number />
+        <h1>Krappa Machine!!!</h1>
+        <Machine kamo1="(╯°□°）╯︵ ┻━┻" kamo2="༼ つ ◕_◕ ༽つ" kamo3="(⌐■_■)" />
       </div>
     );
   }
 }
-
 ReactDom.render(<App />, document.getElementById("root"));
---
+++ src/Machine.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function randomSlot() {
+  return Math.floor(Math.random() * 3);
+}
+
+class Machine extends React.Component {
+  render() {
+    const pos = [this.props.kamo1, this.props.kamo2, this.props.kamo3];
+    const rand1 = randomSlot();
+    const rand2 = randomSlot();
+    const rand3 = randomSlot();
+    console.log(rand1, rand2, rand3);
+    return (
+      <div>
+        <p>
+          {pos[rand1]} {pos[rand2]} {pos[rand3]}
+        </p>
+        <p>
+          {rand1 === rand2 && rand1 === rand3 ? (
+            <img src="https://media1.tenor.com/images/90dc8bb677df8b3d3eab008ea50cefd1/tenor.gif?itemid=15193435" />
+          ) : (
+            <img src="https://media.tenor.com/images/37d36afeee7289b6cfb32a566d0f99f9/tenor.gif" />
+          )}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Machine;
