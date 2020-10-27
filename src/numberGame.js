820cd33f322f5da2c6899a13e87f57e9756a1535
Suaysh Chandra <suyash.chandra24@gmail.com>
2020-10-27 18:31:32 +0530

    Number Game with GIFS :japan:

--- src/numberGame.js
+++ src/numberGame.js
@@ -12,6 +12,11 @@ class Number extends React.Component {
       <div>
         <h1>Your number is...{num}</h1>
         <p>{num === 7 ? "Pog Pog Pog!!!!!" : "Kappa!!!"}</p>
+        {num === 7 ? (
+          <img src="https://media.tenor.com/images/03b867efbe3f723cf2653a196ca6e571/tenor.gif" />
+        ) : (
+          <img src="https://media1.tenor.com/images/e6dbfa8422743bac826cea8d304c0322/tenor.gif?itemid=15954478" />
+        )}
       </div>
     );
   }
