80a43f48b8d78bc7b1eec8ad4349fb1b66355106
Suaysh Chandra <suyash.chandra24@gmail.com>
2020-10-27 17:24:43 +0530

    Simple Functional Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello There!</h1>;
-  }
+function Hello() {
+  return (
+    <div>
+      <h1>Hello there!</h1>
+      <h1>Hello there!</h1>
+      <h1>Hello there!</h1>
+    </div>
+  );
 }
 
 ReactDOM.render(<Hello />, document.getElementById("root"));
