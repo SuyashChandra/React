cdbc13993891b5561a0501ccb0bc95b5d258f8a4
Suaysh Chandra <suyash.chandra24@gmail.com>
2021-02-27 19:42:32 +0530

    Color Overlay Animation

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,19 +3,35 @@ import {CopyToClipboard} from "react-copy-to-clipboard"
 import "./ColorBox.css"
 
 class ColorBox extends Component {
+    constructor(props){
+        super(props);
+        this.state={copied: false};
+        this.changeCopyState = this.changeCopyState.bind(this)
+    }
+    changeCopyState() {
+        this.setState({copied:true}, () =>{
+            setTimeout(() => this.setState({copied: false}), 1500)
+        })
+    }
     render(){
-        const {name, color} = this.props.background
+        const {name, color} = this.props.background;
+        const {copied} = this.state
         return(
-            <CopyToClipboard text={color}>
+            <CopyToClipboard text={color} onCopy={this.changeCopyState}>
 
-            <div style={{background: this.props.background.color}}  className="ColorBox">
-                <div className="copy-container">
-                    <div className="box-content">
-                        <span>{name}</span>
+            <div style={{background: color}}  className="ColorBox">
+                <div style={{background: color}} className={`copy-overlay ${copied && 'show'}`} />
+                    <div className={`copy-msg ${copied && 'show'}`}>
+                        <h1>copied!</h1>
+                        <p>{color}</p>
                     </div>
-                    <button className="copy-button">Copy</button>
-                </div>
-                <span className="see-more">More</span>
+                    <div className="copy-container">
+                        <div className="box-content">
+                            <span>{name}</span>
+                        </div>
+                        <button className="copy-button">Copy</button>
+                    </div>
+                    <span className="see-more">More</span>
             </div>
             </CopyToClipboard>
         )
