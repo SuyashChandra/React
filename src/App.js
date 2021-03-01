de44d1968d375eefe5aa738e219d95296d82ee6f
Suaysh Chandra <suyash.chandra24@gmail.com>
2021-03-01 05:59:14 +0530

    Used Chroma to generate colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { syncBuiltinESMExports } from "module";
 import React, {Component} from "react";
-import Palette from "./Palette"
-import seedColors from "./seedColors"
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import {generatePalette} from "./colorHelpers"
 
 class App extends Component {
     render() {
+        console.log(generatePalette(seedColors[3]))
         return(
             <Palette {...seedColors[4]}></Palette>
         )
