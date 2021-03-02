b5f10605bfd9c2be31fc853a997849adc4354e70
Suaysh Chandra <suyash.chandra24@gmail.com>
2021-03-02 06:57:34 +0530

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { syncBuiltinESMExports } from "module";
 import React, {Component} from "react";
 import Palette from "./Palette";
+import {Route, Switch} from "react-router-dom";
 import seedColors from "./seedColors";
 import {generatePalette} from "./colorHelpers"
 
 class App extends Component {
     render() {
         return(
-            <Palette palette={generatePalette(seedColors[4])}></Palette>
+            <Switch>
+                <Route exact path="/" render={() => <h1>Palette List goes here</h1>} />
+                <Route exact path="/palette/:id" render={() => <h1>Individual Palette list goes here</h1>} />
+            </Switch>
+            // <div>
+            //     <Palette palette={generatePalette(seedColors[4])}></Palette>
+            // </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
+import {BrowserRouter} from "react-router-dom"
 import App from "./App"
 
-class Index extends React.Component {
-  render() {
-    return (
-      <div>
-        <App></App>
-      </div>
-    );
-  }
-}
-ReactDom.render(<Index />, document.getElementById("root"));
+
+ReactDom.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
