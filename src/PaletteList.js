834b41fc6d5af05249dbfcea717008c74d8691f9
Suaysh Chandra <suyash.chandra24@gmail.com>
2021-03-02 08:35:17 +0530

    Using Materail UI JSS for style

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import MiniPalette from "./MiniPalette";
+
 class PaletteList extends Component {
   render() {
     const { palettes } = this.props;
     return (
       <div>
+        <MiniPalette />
+
         <h1>Here goes the Palette List</h1>
         {palettes.map((palette) => (
           <p>
